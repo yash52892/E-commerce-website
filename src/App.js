d01c0e9a7beaf6ca9bc353ccfc8506b96c0a9b4c
Yashwanth kumar R <97838432+yash52892@users.noreply.github.com>
2023-12-09 18:34:49 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import Sub from './components/Sub';
 import Items from './components/Items';
 import Footer from './components/Footer';
 import { Button } from 'react-bootstrap';
-
+import Cart from './components/Cart';
+import { useState } from 'react';
 
 const App=()=>{
   const productsArr = [
@@ -30,13 +31,18 @@ const App=()=>{
     imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%204.png',
     }
     ]
+    const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   return (
     <React.Fragment>
-    <Header/>
+    <Header on={handleShow}/>
+    <Cart product={productsArr} show={show} off={handleClose}/>
     <Sub/>
     <Items items={productsArr}/>
-    <Button variant="outline-secondary" style={{ float: "centre", padding:"15px", backgroundcolor:"#777"}}>See the Cart</Button>
+    <Button variant="outline-secondary" style={{marginLeft:"725px"}} onClick={handleShow}>See the Cart</Button>
     <Footer/>
     </React.Fragment>
   );
