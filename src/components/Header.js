614708eb801b13a2897345c2c8e2dfcbb4e46cf4
Yashwanth kumar R <97838432+yash52892@users.noreply.github.com>
2023-12-09 18:33:14 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,28 @@
-import Cart from "./Cart";
-import { Nav, Navbar, Container } from "react-bootstrap";
+import { Nav, Navbar, Container, Badge } from "react-bootstrap";
 
-const Header = () => {
+const Header = (props) => {  
+const handleShow=()=>{
+  props.on();
+}
+
   return (
+    
     <Navbar bg="dark" data-bs-theme="dark" className="justify-content-center">
       <Container>
         <Nav className="me-auto">
+        
           <Nav.Link href="#home">Home</Nav.Link>
           <Nav.Link href="#features">Store</Nav.Link>
           <Nav.Link href="#pricing">About</Nav.Link>
-          <Cart />
+          
+          <button variant="dark" onClick={handleShow}>
+            Cart
+            <Badge>0</Badge>
+          </button>
         </Nav>
       </Container>
     </Navbar>
+   
   );
 };
 
