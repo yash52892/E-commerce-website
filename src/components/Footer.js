ec71fe3a726c31b2dc5ea40777494a84ebf75921
Yashwanth kumar R <97838432+yash52892@users.noreply.github.com>
2023-12-09 18:34:23 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,38 @@
 //import { Card } from "react-bootstrap";
 
-const Footer=()=>{
-    return(
-       <footer style={{background:'#56ccf2'}}>
-        <h1>The Generics</h1>
-       </footer>
-    )
-}
+const Footer = () => {
+  return (
+    <footer style={{ background: "#56ccf2" }}>
+      <div
+        style={{
+          textAlign: "center",
+          color: "white",
+          width: "50%",
+          fontSize: "50px",
+          fontWeight: "bold",
+        }}
+      >
+        The Generics
+      </div>
+      <div>
+        <a href="youtube.com">
+          <img src="../Images/youtube.jpg" style={{ width: "40px" }} alt="" />
+        </a>
+
+        <a href="spotify.com">
+          <img src="../Images/Spotify.png" style={{ width: "40px" }} alt="" />
+        </a>
+
+        <a href="fb.com">
+          <img
+            src="../Images/Facebook Logo.png"
+            style={{ width: "40px" }}
+            alt=""
+          />
+        </a>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
