ee1e9f1d3d6d752c3afb566a4fe3369385b8caf5
Yashwanth kumar R <97838432+yash52892@users.noreply.github.com>
2023-12-09 18:33:32 +0530

    Update Cart.js

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,10 +1,35 @@
-import { Badge, Button } from "react-bootstrap";
-const Cart=()=>{
+import { Modal, Badge, Button} from "react-bootstrap";
+import ListGroup from 'react-bootstrap/ListGroup';
+
+
+const Cart=(props)=>{
+    console.log(props.product);
+    const handleClose=()=>{
+        props.off();
+    }
     return(
-    <Button variant="dark">Cart
-    <Badge>0
-    </Badge>
-    </Button>
+        <Modal show={props.show} onHide={handleClose}>
+        <h2>Cart</h2>
+       {props.product.map((item) => (
+        <ListGroup as="ol" numbered>
+        <ListGroup.Item as="li" className="d-flex justify-content-between align-items-start">
+          <div className="ms-2 me-auto">
+            <div className="fw-bold">Subheading</div>
+            {item.title}
+          </div>
+          <button>+</button>
+          <button>-</button>
+          <Badge bg="primary" pill>
+          Rs.{item.price}
+          </Badge>
+        </ListGroup.Item>
+        </ListGroup>
+        ))}
+        <div>
+        <Button variant="secondary" size="sm">Close</Button>
+        <Button variant="secondary" size="sm">Purchase</Button>
+        </div>
+        </Modal>
     )
 }
 
