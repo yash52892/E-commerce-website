c6902101991fde4d649ce32691d3f0c50b7fa0d1
Yashwanth kumar R <97838432+yash52892@users.noreply.github.com>
2023-12-13 18:28:19 +0530

    Update display.js

--- src/components/display.js
+++ src/components/display.js
@@ -1,15 +1,27 @@
-const Display=(props)=>{
-    
-    return(
-      
-        <div id="colour-container" style={{width:"flex"}}>
-            <div>
-            <h3>{props.title}</h3>
-            <img alt={props.title} src={props.url} />    
-            </div>
-      </div>
+import React, { useContext } from "react";
+import {Button } from "react-bootstrap";
+import CartContext from "../Store/CartContext";
 
-    )
+const Display=(props)=>{
+    const cart=useContext(CartContext);
+const addCartHandler=()=>{
+    cart.addItem(props);
+    console.log(props);
+}
+return(
+    <section className="container">
+      <div style={{display: "grid", boxSizing: "border-box"}}>
+        <div>
+        <h3>{props.title}</h3>
+        <img alt={props.title} src={props.url}/>  
+        </div>
+        <div style={{display: "flex", justifyContent:"space-between"}}>
+        <span>Rs. {props.price} </span> 
+        <Button variant="primary" size="sm" style={{display:"flex"}} onClick={addCartHandler}>Add to Cart</Button>
+        </div>
+      </div>
+    </section>
+)
 }
 
 export default Display;
