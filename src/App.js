22f833bcc8901383a0ff549dc999fdd6e80fec5a
Yashwanth kumar R <97838432+yash52892@users.noreply.github.com>
2023-12-27 10:59:33 +0530

    fetch movies when reload

--- src/App.js
+++ src/App.js
@@ -1,30 +1,61 @@
-import './App.css';
-import React from 'react';
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from './components/Home';
-import Product from './components/Product';
-import Root from './Root';
-import CartProvider from './Store/CartProvide';
-import About from './components/About';
+import React, { useEffect } from "react";
+import { useState } from "react";
 
-const App=()=>{
+import MoviesList from "./components/MoviesList";
+import "./App.css";
+
+function App() {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+ 
   
-  const route=createBrowserRouter(
-    [{path: '/', 
-      element: <Root/>, 
-      children:[
-        {path: '/home', element: <Home/>},
-        {path: '/product', element: <Product/>},
-        {path: '/about', element: <About/>}
-    ]}]
-    );
+
+  const fetchmoviesHandler= async ()=>{
+    setIsLoading(true);
+    setError(null);
+    try {
+      let response = await fetch("https://swapi.dev/api/films");
+      if (!response.ok) {
+        throw new Error("Something went wrong... Retrying");
+      }
+      const data = await response.json();
+      const newMovies = data.results.map((item) => {
+        return {
+          id: item.episode_id,
+          title: item.title,
+          releaseDate: item.release_date,
+          openingText: item.opening_crawl,
+        };
+      });
+      setMovies(newMovies);
+      setIsLoading(false);
+    } catch (error) {
+      setError(error);
+      setIsLoading(false);
+    }
+  }
+  useEffect(()=>{
+        fetchmoviesHandler();
+      },[]);
 
   return (
-    <CartProvider>
-      <RouterProvider router={route}/>
-      </CartProvider>
+    <React.Fragment>
+      <section>
+        <button onClick={fetchmoviesHandler}>Fetch Movies</button>
+      </section>
+      <section className="section">
+        {isLoading && !error && <section className="loader"></section>}
+        {!error && <MoviesList movies={movies} />}
+        {error && <p>{error.message}</p>}
+        {/* {isLoading && !error && (
+          <button className="button" onClick={()=>{setStop(true)}}>
+            Cancel
+          </button>
+        )} */}
+      </section>
+    </React.Fragment>
   );
-  
 }
 
 export default App;
