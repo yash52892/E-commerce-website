9421593107d79709f5185c43bb40c31fed5d0315
Yashwanth kumar R <97838432+yash52892@users.noreply.github.com>
2024-01-03 18:28:52 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,50 +1,68 @@
-import './App.css';
-import React from 'react';
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from './components/Home';
-import Product from './components/Product';
-import Root from './Root';
-import CartProvider from './Store/CartProvide';
-import About from './components/About';
-import Contact from './components/Contact';
+import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route,Routes } from "react-router-dom";
+import Home from "./components/Home";
+import Product from "./components/Product";
+import Root from "./Root";
+import CartProvider from "./Store/CartProvide";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Productdetails from "./components/Productdetails";
 
-const App=(props)=>{
-  const handlerproShow=()=>{
+const App = (props) => {
+  const handlerproShow = () => {
     props.onnavShow();
-  }
-  const handle=async (obj)=>{
+  };
+  const handle = async (obj) => {
     try {
-      const res=await fetch('https://ecommerce-5629f-default-rtdb.firebaseio.com/feedback.json', {
-      method: 'POST',
-      body: JSON.stringify(obj),
-      headers:{
-        'Content-Type': 'application/json'
-      }
-    });
-    const data=await res.json();
-    console.log(data); 
+      const res = await fetch(
+        "https://ecommerce-5629f-default-rtdb.firebaseio.com/feedback.json",
+        {
+          method: "POST",
+          body: JSON.stringify(obj),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      const data = await res.json();
+      console.log(data);
     } catch (error) {
       console.log(error);
     }
-  }
-  
-  const route=createBrowserRouter(
-    [{path: '/', 
-      element: <Root/>, 
-      children:[
-        {path: '/', element: <Home/>},
-        {path: '/product', element: <Product onProShow={handlerproShow}/>},
-        {path: '/about', element: <About/>},
-        {path: '/contact', element:<Contact handleContact={handle}/>}
-    ]}]
-    );
+  };
+
+  // const route=createBrowserRouter(
+  //   [{path: '/',
+  //     element: <Root/>,
+  //     children:[
+  //       {path: '/', element: <Home/>},
+  //       {path: '/product', element: <Product onProShow={handlerproShow}/>,
+  //       children:[
+  //         {path:'/product/productdetails', element:<Productdetails/>}
+  //       ]},
+  //       {path: '/about', element: <About/>},
+  //       {path: '/contact', element:<Contact handleContact={handle}/>}
+  //   ]}]
+  //   );
 
   return (
     <CartProvider>
-      <RouterProvider router={route}/>
-      </CartProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Root />}>
+          <Route path="/home" element={<Home/>}/>
+          <Route exact path="/product" element={<Product onProShow={handlerproShow} />}/>  
+          <Route path="/product/:pId" element={<Productdetails item={props}/>}/>
+          <Route path="/about" element={ <About />}/>
+          <Route path="/contact" element={<Contact handleContact={handle} />}/>
+          </Route>
+        </Routes>
+      </Router>
+
+      {/* <RouterProvider router={route}/> */}
+    </CartProvider>
   );
-  
-}
+};
 
 export default App;
