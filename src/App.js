9ea0c6367d64617f682b42fa274c7d43ef2adb49
Yashwanth kumar R <97838432+yash52892@users.noreply.github.com>
2024-01-07 16:28:52 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import "./App.css";
-import React from "react";
-import { BrowserRouter as Router, Route,Routes } from "react-router-dom";
-import Home from "./components/Home";
-import Product from "./components/Product";
+import React, { useContext } from "react";
+import { BrowserRouter as Router, Route,Routes, Navigate } from "react-router-dom";
+import Home from "./pages/Home";
+import Product from "./pages/Product";
 import Root from "./Root";
 import CartProvider from "./Store/CartProvide";
-import About from "./components/About";
-import Contact from "./components/Contact";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 import Productdetails from "./components/Productdetails";
+import Login from "./pages/Login";
+import TokenContext from "./Store/TokenContext";
 
 const App = (props) => {
+  const tok=useContext(TokenContext);
+  console.log(tok.isLoggedin);
   const handlerproShow = () => {
     props.onnavShow();
   };
@@ -32,36 +36,26 @@ const App = (props) => {
     }
   };
 
-  // const route=createBrowserRouter(
-  //   [{path: '/',
-  //     element: <Root/>,
-  //     children:[
-  //       {path: '/', element: <Home/>},
-  //       {path: '/product', element: <Product onProShow={handlerproShow}/>,
-  //       children:[
-  //         {path:'/product/productdetails', element:<Productdetails/>}
-  //       ]},
-  //       {path: '/about', element: <About/>},
-  //       {path: '/contact', element:<Contact handleContact={handle}/>}
-  //   ]}]
-  //   );
-
   return (
+    
     <CartProvider>
       <Router>
         <Routes>
           <Route path="/" element={<Root />}>
-          <Route path="/home" element={<Home/>}/>
-          <Route exact path="/product" element={<Product onProShow={handlerproShow} />}/>  
-          <Route path="/product/:pId" element={<Productdetails item={props}/>}/>
+          <Route path="/home"  element={<Home/>}/>
+          <Route path="/product" element={tok.isLoggedin ? (<Product onProShow={handlerproShow}/>) :
+           (<Navigate replace to="/login" element={ <Login />}/>)}/>
+           <Route path="/product/:pId" element={tok.isLoggedin ? (<Route path="/product/:pId" element={<Productdetails item={props}/>}/>) :
+           (<Navigate replace to="/login" element={ <Login />}/>)}/>
           <Route path="/about" element={ <About />}/>
+          {!tok.isLoggedin && (<Route path="/login" element={ <Login />}/>)}
           <Route path="/contact" element={<Contact handleContact={handle} />}/>
+          <Route path="*" element={<Navigate replace to="/"/>}/>
           </Route>
         </Routes>
       </Router>
-
-      {/* <RouterProvider router={route}/> */}
     </CartProvider>
+   
   );
 };
 
