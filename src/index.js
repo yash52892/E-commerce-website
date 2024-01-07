fe4f6deef1242335880dfc18d18bbe8f1b1beab8
Yashwanth kumar R <97838432+yash52892@users.noreply.github.com>
2024-01-07 16:24:27 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import TokenProvider from "./Store/TokenProvider";
 
 //import "../node_modules/react-bootstrap/dist/react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -9,7 +10,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+  <TokenProvider>
     <App />
+  </TokenProvider>
   </React.StrictMode>
 );
 
