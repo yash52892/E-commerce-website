072c5f3b4c31887d13a6928b64c2913f73b02b85
Yashwanth kumar R <97838432+yash52892@users.noreply.github.com>
2024-01-13 17:33:22 +0530

    Update TokenProvider.js

--- src/Store/TokenProvider.js
+++ src/Store/TokenProvider.js
@@ -3,14 +3,19 @@ import TokenContext from "./TokenContext";
 
 const TokenProvider=(props)=>{
     const initToken=localStorage.getItem("id");
+    const initEmail=localStorage.getItem("email");
+
     const [token, setToken]=useState(initToken);
+    const [emailId, setEmailid]=useState(initEmail);
 
     const isLogged=!!token;
-    console.log(isLogged)
+  
 
-    const login=(id)=>{
+    const login=(id, email)=>{
         setToken(id);
         localStorage.setItem("id",token);
+        setEmailid(email);
+        localStorage.setItem("email",emailId);
     }
 
     const logout=()=>{
@@ -23,7 +28,8 @@ const TokenProvider=(props)=>{
         token: token,
     loginHandler:login,
     logoutHandler:logout,
-    isLoggedin: isLogged
+    isLoggedin: isLogged,
+    emailId:emailId
     }
 
     return(
