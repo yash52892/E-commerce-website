9ee0179e3f44f2b041da0467a919e5c3f21092e6
Yashwanth kumar R <97838432+yash52892@users.noreply.github.com>
2024-01-13 17:36:36 +0530

    Update Productdetails.js

--- src/components/Productdetails.js
+++ src/components/Productdetails.js
@@ -1,19 +1,48 @@
 import { useParams } from "react-router-dom";
-import {productsArr} from "./Product";
+import {productsArr} from "../pages/Product";
+import classes from "./Display.module.css";
+import { Button } from "react-bootstrap";
+import { useRef, useContext } from "react";
+import CartContext from "../Store/CartContext";
 
 const Productdetails=(props)=>{
    const param=useParams();
+
+   const cart = useContext(CartContext);
+  const q=useRef(1);
+  
+  const addCartHandler = () => {
+   const quantity=q.current.value;
+    cart.addItem(props,quantity);
+    
+  };
    
    const item=productsArr.filter((product)=>{
       return product.price===parseInt(param.pId);
    })
 
      return(
-      <>
-      <img src={item[0].imageUrl} alt="text"></img>
-     <p>{item[0].price}</p> 
-     <p>{item[0].title}</p>
-     </>
+      <section className={classes.container}>
+      <img src={item[0].imageUrl} alt="text"/>
+      <div>
+     <h4>Rs. {item[0].price}</h4> 
+     <h3>{item[0].title}</h3>
+     <div className={classes.item}>
+        <form className={classes.item}>
+          <label>Quantity</label>
+          <input type="number" defaultValue={1} ref={q}></input>
+        </form>
+      </div>
+     <Button
+          variant="primary"
+          size="sm"
+          style={{ display: "flex" }}
+          onClick={addCartHandler}
+        >
+          Add to Cart
+        </Button>
+     </div>
+     </section>
      )
 }
 
