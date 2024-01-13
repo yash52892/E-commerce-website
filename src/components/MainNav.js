ab289ee602003920083c7fc31934b176dced42ea
Yashwanth kumar R <97838432+yash52892@users.noreply.github.com>
2024-01-13 17:37:22 +0530

    Update MainNav.js

--- src/components/MainNav.js
+++ src/components/MainNav.js
@@ -1,30 +1,42 @@
-import { Nav, Navbar, Container, Badge } from "react-bootstrap";
-import CartContext from "../Store/CartContext";
-import Cart from "./Cart";
-import Sub from "./Sub";
+import { Nav, Navbar, Container } from "react-bootstrap";
+import CartContext from "../../Store/CartContext";
+import Cart from "../Cart/Cart";
+import Sub from "../Sub";
 import { Link } from "react-router-dom";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import classes from "./MainNav.module.css";
+import TokenContext from "../../Store/TokenContext";
 
 
 const MainNav = (props) => {
     const cart = useContext(CartContext);
-  const cartHideHandler=()=>{
-    props.onnavHide();
+    const tok=useContext(TokenContext);
+    const email_id=tok.emailId.replace(/[^\w ]/g,'');
+
+    const[items, setItems]=useState([])
+
+  const handleShow=async ()=>{
+    const res=await fetch(`https://crudcrud.com/api/f462e23384b74d89a06cf27a32e60caa/cart${email_id}`);
+    const response=await res.json();
+    setItems(response)
+    cart.cartShow();
+    // console.log(data);
   }
+ 
   return (
     <>
       <Navbar bg="dark" data-bs-theme="dark">
-        <Container  >
+        <Container>
           <Nav className={classes.meauto}>
             <Link to="/home">Home</Link>
             <Link to="/product">Product</Link>
             <Link to="/about">About</Link>
-      <Link to="/contact">Contact Us</Link>
-            <button variant="dark" onClick={props.onnavShow}>
+            <Link to="/login">Login</Link>
+            <Link to="/contact">Contact Us</Link>
+            <button variant="dark" onClick={handleShow}>
               Cart
-              <Badge>{cart.items.length}</Badge>
-              <Cart cartshow={props.navshow} onNavHide={cartHideHandler}/>
+              {/* <Badge>{cart.items.length}</Badge> */}
+              <Cart cartItems={items}/>
             </button>
           </Nav>
         </Container>
